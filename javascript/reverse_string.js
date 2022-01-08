aef4a0fa2766e54dbc2f6aa76aa908ebec36929c
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-01-08 11:50:15 -0600

    completed assignment

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,13 +1,21 @@
 function reverseString(str) {
-  // type your code here
+  let newString = ""
+  for (let i = str.length -1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
 }
+
+
+
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'ih'");
   console.log("=>", reverseString("hi"));
 
-  console.log("");
+  console.log("Expecting: airam");
+  console.log("=>", reverseString("maria");
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
@@ -16,4 +24,9 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+// starting at the last index, add each letter in turn to the new string
+
 // And a written explanation of your solution
+// set new string to empty string
+// for each loop, starting at the last index and decrementing to go backwards
+// add each letter in turn to the new string to be returned. 
